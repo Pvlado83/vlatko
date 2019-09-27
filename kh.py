6b04d42e6be467f92d9cdd0704ab3a3d66ed1ec5
Pvlado83 <55879856+Pvlado83@users.noreply.github.com>
2019-09-27 13:55:38 +0200

    test
    testtestetets

---
+++ kh.py
@@ -0,0 +1,11 @@
+import sys
+
+if (len(sys.argv) < 3):
+    print("you gave too little", len(sys.argv), "commandline arguments. Should be 3, but if it is les or more then answer should be given")
+    sys.exit()
+elif (len(sys.argv) > 3):
+    print("you gave too many commandline argumrnts. should be 3")
+    sys.exit()
+print("Hello {}. Welcome to {} tutorial".format(sys.argv[1], sys.argv[2]))
+print(len(sys.argv))
+print(len(sys.argv[0]) + len(sys.argv[1]) + len(sys.argv[2]))
